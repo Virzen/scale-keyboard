3a3f430c251a293b037d8ae7ea82564be42951a1
Virzen <wiktor.czajkowski@gmail.com>
2016-04-03 19:48:24 +0200

    Make voice interface more fluent and prototypal button generation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-(function () {
+(function (teoria) {
 	'use strict';
 
 	const audioModule = (function () {
@@ -12,6 +12,7 @@
 		const voice = function ({ frequency = 440, type = 'sine', volume = 0.5 }) {
 			const osc = ctx.createOscillator();
 			const gainNode = ctx.createGain();
+			let voice = {};
 
 			osc.frequency.value = frequency;
 			osc.type = type;
@@ -24,22 +25,91 @@
 
 			const start = function () {
 				gainNode.gain.value = volume;
+				return voice;
 			};
 
 			const stop = function () {
 				gainNode.gain.value = 0;
+				return voice;
 			};
 
-			return {
+			voice = {
 				frequency: frequency,
 				start: start,
 				stop: stop
 			};
+
+			return voice;
 		};
 
+		// audioModule public interface
 		return {
 			voice: voice
 		};
 	}());
 
-}());
+	const settingsModule = (function () {
+		const scales = [ 'major', 'minor' ];
+		const oscillators = [ 'sine', 'triangle', 'sawtooth', 'square' ];
+
+		const settings = {
+			scale: scales[0],
+			oscillator: oscillators[0],
+			volume: 0.5
+		};
+
+		const setScale = function (scale) {
+			if (scales.indexOf(scale) !== -1) {
+				settings.scale = scale;
+				return settings;
+			}
+		};
+
+		const setOscillatorType = function (type) {
+			if (oscillators.indexOf(type) !== -1) {
+				settings.oscillator = type;
+				return settings;
+			}
+		};
+
+		const setVolume = function (volume) {
+			if (volume >= -1 && volume <= 1) {
+				settings.volume = volume;
+				return settings;
+			}
+		};
+
+		return {
+			get settings() {
+				return Object.create(settings);
+			},
+			setScale: setScale,
+			setOscillatorType: setOscillatorType,
+			setVolume: setVolume
+		};
+	}());
+
+	const musicNotationModule = (function () {
+		
+	}());
+
+	const notes = teoria.note('c')
+		.scale('major')
+		.simple()
+		.map(s => s + '3');
+	notes.push(notes[0].replace('3', '4'));
+	const freqs = notes.map(note => teoria.note(note).fq());
+	const voices = freqs.map(freq => audioModule.voice({ frequency: freq }));
+
+	voices.forEach(voice => {
+		const button = document.createElement('button');
+		button.textContent = Math.round(voice.frequency * 100, 2) / 100;
+
+		button.addEventListener('mousedown', voice.start, false);
+		button.addEventListener('mouseup', voice.stop, false);
+
+		document.body.appendChild(button);
+	});
+
+}(teoria));
+/* global teoria */
